36bcc57c67deb9837ce5badba78768e25e3378dc
Felipe Goulart <flpgoulart@gmail.com>
2018-02-16 11:52:41 +0000

    Changing double quotes to simple quotes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'api_version_constraint'
 Rails.application.routes.draw do
   #comentado a linha abaixo para que o devise_for não atrapalhe as rotas que definimos abaixo
   # devise_for :users
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   # namespace é utilizado para agrupar os Controles (controllers), de forma a melhorar o codigo
@@ -11,12 +11,12 @@ Rails.application.routes.draw do
   # a constraints neste caso, exige que o subdomínio seja api, ficando dessa forma api.nome-site.com
   # o comando "path: '/'" inibe a necessidade de informar o caminho, ou seja, antes do comando deveria ser
   # api.nome-site.com/api e com este comando ele entende como api.nome-site.com
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: "/" do
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
     # este caso é um agrupamento dentro do namespace api, para controlar as versões
     # o comando path neste caso, evita que o usuário tenha que informar a versão na URL
     # o controle da versão está pelo cabeçalho http, informando a versão na requisição esse dado
     # o ApiVersionConstraint é uma classe criada, e está localizada na pasta "lib"
-    namespace :v1, path: "/", constraints: ApiVersionConstraint.new(version: 1, default: true) do
+    namespace :v1, path: '/', constraints: ApiVersionConstraint.new(version: 1, default: true) do
       #irá criar a rota para o users e somente para a action show
       resources :users, only: [:show]
     end
--- spec/requests/api/v1/users_spec.rb
+++ spec/requests/api/v1/users_spec.rb
@@ -7,32 +7,32 @@ RSpec.describe 'Users API', type: :request do
   let!(:user) { create(:user) }
   let(:user_id) { user.id }
 
-  before { host! "api.taskmanager.dev" }
+  before { host! 'api.taskmanager.dev' }
 
-  describe "GET /users/:id" do
+  describe 'GET /users/:id' do
     before do
-      headers = { "Accept" => "application/vnd.taskmanager.v1" }
+      headers = { 'Accept' => 'application/vnd.taskmanager.v1' }
       get "/users/#{user_id}", params: {}, headers: headers
     end
 
       # este contexto serve para agrupar quando o usuário existir
-      context "when the user exists" do
-        it "returns the user" do
+      context 'when the user exists' do
+        it 'returns the user' do
           #nesta instrução, eu pego o resultado da minha requisição GET e converto para JSON o Body da mensagem
           user_response = JSON.parse(response.body)
           # Espero que o ID contido no Body seja igual ao user_id definido no inicio do comando
-          expect(user_response["id"]).to eq(user_id)
+          expect(user_response['id']).to eq(user_id)
         end
         
         #compara se deu tudo certo na requisição, esperado para o código 200
-        it "returns status code 200" do
+        it 'returns status code 200' do
           expect(response).to have_http_status(200)
         end 
       end 
 
-      context "when the user does not exists" do
+      context 'when the user does not exists' do
         let(:user_id) { 10000 }
-        it "returns status code 404" do
+        it 'returns status code 404' do
           expect(response).to have_http_status(404)
         end
       end 
