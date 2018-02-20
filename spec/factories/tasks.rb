4d755e36d255cb6f266cc02e259c124af80d4dda
Felipe Goulart <flpgoulart@gmail.com>
2018-02-20 20:04:04 +0000

    Adding tests and improvements for task model

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do 
+    factory :task do
+        title { Faker::Lorem.sentence }
+        description { Faker::Lorem.paragraph }
+        deadline { Faker::Date.forward }
+        done false 
+        user_id "1"
+    end 
+
+end 
