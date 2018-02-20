2d99a1335adea6b5347007ce93af88f89b3fd410
Felipe Goulart <flpgoulart@gmail.com>
2018-02-20 11:57:52 +0000

    Adding #authenticate_with_token method

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -42,7 +42,8 @@ RSpec.configure do |config|
 
   config.include FactoryGirl::Syntax::Methods
 
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
