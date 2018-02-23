121d45603121bc41e79b3d4b6ae208d6d80b726e
Felipe Goulart <flpgoulart@gmail.com>
2018-02-23 13:57:26 +0000

    Adding create action to the tasks controller with tests

--- spec/requests/api/v1/tasks_spec.rb
+++ spec/requests/api/v1/tasks_spec.rb
@@ -45,4 +45,49 @@ RSpec.describe 'Task API', type: :request do
         
     end
     
+    describe 'POST /tasks' do
+        before do
+            post '/tasks', params: { task: task_params }.to_json, headers: headers
+        end
+        
+        context 'when the params are valid' do
+            let(:task_params) { attributes_for(:task) } 
+
+            it 'returns status code 201' do
+                expect(response).to have_http_status(201)
+            end
+            
+            it 'save the task in the database' do
+                expect( Task.find_by( title: task_params[:title] ) ).not_to be_nil 
+            end
+
+            it 'returns the json for created task' do
+                expect(json_body[:title]).to eq(task_params[:title])
+            end
+
+            it 'assigns the created task to the current user' do
+                expect(json_body[:user_id]).to eq(user.id)
+            end
+        end
+
+        context 'when the params are invalid' do
+            let(:task_params) { attributes_for(:task, title: ' ') }
+
+            it 'returns status code 422' do
+                expect(response).to have_http_status(422)
+            end
+
+            it 'does not save the task in the database' do
+                expect( Task.find_by( title: task_params[:title] ) ).to be_nil 
+            end
+
+            # neste teste, ele especifica que quer especificamente o erro no titulo
+            it 'returns the json errors for title' do
+                expect(json_body[:errors]).to have_key(:title)
+            end
+        end
+        
+        
+    end
+    
 end
