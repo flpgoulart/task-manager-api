3219a69b77d84623ac3d483de0be6e226d626739
Felipe Goulart <flpgoulart@gmail.com>
2018-03-12 16:53:59 +0000

    updating the Users API

--- spec/requests/api/v2/users_spec.rb
+++ spec/requests/api/v2/users_spec.rb
@@ -5,30 +5,33 @@ RSpec.describe 'Users API', type: :request do
   # o let com a exclamação força a ser executado o comando, neste caso para instanciar o user
   # o create cria o objeto no banco de dados, diferente do build que só instancia em memória
   let!(:user) { create(:user) }
-  let(:user_id) { user.id }
-
+  let!(:auth_data) { user.create_new_auth_token }
   #esta definição é para não repetirmos alguns trechos de código
   let(:headers) do
     {
       'Accept' => 'application/vnd.taskmanager.v2',
       'Content-Type' => Mime[:json].to_s,
-      'Authorization' => user.auth_token
+      'access-token' => auth_data['access-token'],
+      'uid' => auth_data['uid'],
+      'client' => auth_data['client']
     }
   end
 
   before { host! 'api.taskmanager.dev' }
 
   #para recuperar um dado -show
-  describe 'GET /users/:id' do
-    before do
-      get "/users/#{user_id}", params: {}, headers: headers
-    end
+  describe 'GET /auth/validate_token' do
 
-      # este contexto serve para agrupar quando o usuário existir
-      context 'when the user exists' do
-        it 'returns the user' do
+    # este contexto serve para agrupar quando o usuário existir
+      context 'when the request headers are valid' do
+
+        before do
+          get '/auth/validate_token', params: {}, headers: headers
+        end
+        
+        it 'returns the user id' do
           # Espero que o ID contido no Body seja igual ao user_id definido no inicio do comando
-          expect(json_body[:data][:id].to_i).to eq(user_id)
+          expect(json_body[:data][:id].to_i).to eq(user.id)
         end
         
         #compara se deu tudo certo na requisição, esperado para o código 200
@@ -37,30 +40,34 @@ RSpec.describe 'Users API', type: :request do
         end 
       end 
 
-      context 'when the user does not exists' do
-        let(:user_id) { 10000 }
-        it 'returns status code 404' do
-          expect(response).to have_http_status(404)
+      context 'when the request headers are not valid' do
+        before do
+          headers['access-token'] = "invalid_token"
+          get '/auth/validate_token', params: {}, headers: headers
+        end
+        
+        it 'returns status code 401' do
+          expect(response).to have_http_status(401)
         end
       end 
   end 
   
   #para enviar um novo dado -create
-  describe 'POST /users' do
+  describe 'POST /auth' do
     before do
-      post '/users', params: { user: user_params }.to_json, headers: headers
+      post '/auth', params: user_params.to_json, headers: headers
     end 
 
     context 'when the request params are valid' do
       #attributes_for pertence a biblioteca do FactoryGirl
       let(:user_params) { attributes_for(:user) }  
 
-      it 'returns status code 201' do 
-        expect(response).to have_http_status(201)
+      it 'returns status code 200' do 
+        expect(response).to have_http_status(200)
       end
 
       it 'returns json data for the created user' do
-        expect(json_body[:data][:attributes][:email]).to eq(user_params[:email]) 
+        expect(json_body[:data][:email]).to eq(user_params[:email]) 
       end
       
     end
@@ -79,10 +86,10 @@ RSpec.describe 'Users API', type: :request do
   end
 
   #para atualizar um dado -update
-  describe 'PUT /users/:id' do
+  describe 'PUT /auth' do
 
     before do
-      put "/users/#{user_id}", params: { user: user_params }.to_json, headers: headers
+      put '/auth', params: user_params.to_json, headers: headers
     end 
 
     context 'when the request params are valid' do
@@ -93,7 +100,7 @@ RSpec.describe 'Users API', type: :request do
       end
 
       it 'returns the json data for the update user' do
-        expect(json_body[:data][:attributes][:email]).to eq(user_params[:email])
+        expect(json_body[:data][:email]).to eq(user_params[:email])
       end 
     end
 
@@ -112,13 +119,13 @@ RSpec.describe 'Users API', type: :request do
   end
   
   #para excluir um dado -destroy
-  describe 'DELETE /users/:id' do
+  describe 'DELETE /auth' do
     before do
-      delete "/users/#{user_id}", params: {}, headers: headers
+      delete '/auth', params: {}, headers: headers
     end
     
-    it 'returns status code 204' do
-      expect(response).to have_http_status(204) 
+    it 'returns status code 200' do
+      expect(response).to have_http_status(200) 
     end
 
     it 'removes the user from database' do 
