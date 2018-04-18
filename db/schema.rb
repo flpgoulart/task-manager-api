2d6360bfbfe5e422b3fe9fe46175caf926336442
Felipe Goulart <flpgoulart@gmail.com>
2018-04-18 08:35:33 +0000

    adding task type model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312093633) do
+ActiveRecord::Schema.define(version: 20180418083450) do
+
+  create_table "task_types", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
